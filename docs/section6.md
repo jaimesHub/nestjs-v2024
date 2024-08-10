0a6c9f2a83fb345b3fefebed0e30f17179cca0c1
jaimesHub <duongquockhanh230596@gmail.com>
2024-08-10 13:51:26 +0700

    #21 - Create Schema (Model)

--- docs/section6.md
+++ docs/section6.md
@@ -9,3 +9,9 @@ Goal
 ## Generate resources
 - `nest g resource users --no-spec`
 - "@nestjs/mapped-types": "1.2.2"
+
+## Create Schema (Models)
+- Add database name in .env
+- Model injection
+    - create schema
+    - validate schema
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ isRequired: true })
+  email: string;
+
+  @Prop({ isRequired: true })
+  password: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
