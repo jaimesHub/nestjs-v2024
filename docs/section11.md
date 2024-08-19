ef6a80ce85e751c135ad011589f2b687d5340ea3
jaimesHub <duongquockhanh230596@gmail.com>
2024-08-19 12:47:47 +0700

    #55 - Update User Type (JWT)

--- docs/section11.md
+++ docs/section11.md
@@ -1,3 +1,18 @@
 # Modules Company
 
 - create a company
+- update user type (jwt)
+    - Create user interface: users/users.interface.ts
+- Update JWT token (issue token when login)
+    - auth.service.ts
+        - login()
+- Update decode token
+    - jwt.strategy.ts
+- Update message error
+    - local.strategy.ts
+    - jwt-auth.guard.ts
+- Move /login => auth controller (auth.controller)
+
+- test
+    - login
+    
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,38 +1,19 @@
-import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ConfigService } from '@nestjs/config';
+import {
+  Controller,
+  // Get, 
+  // Post, 
+  // Render, 
+  // Request, 
+  // UseGuards 
+} from '@nestjs/common';
+// import { AppService } from './app.service';
+// import { ConfigService } from '@nestjs/config';
 // import { AuthGuard } from '@nestjs/passport';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
+// import { LocalAuthGuard } from './auth/local-auth.guard';
+// import { AuthService } from './auth/auth.service';
 // import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { Public } from './decorator/customize';
+// import { Public } from './decorator/customize';
 
 @Controller()
 // Consumer
-export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private configService: ConfigService,
-    private authService: AuthService,
-  ) { }
-
-  @Public()
-  @UseGuards(LocalAuthGuard)
-  @Post('/login')
-  handleLogin(@Request() req) {
-    return this.authService.login(req.user);
-  }
-
-  // @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
-
-  @Public()
-  // @UseGuards(JwtAuthGuard)
-  @Get('profile1')
-  getProfile1(@Request() req) {
-    return req.user; // null
-  }
-}
+export class AppController { }
